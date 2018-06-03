516b9ef71a6f1171fd8cbac71d2cabb005441313
Stephen Krewson <stephen.krewson@gmail.com>
2018-06-02 23:59:44 -0400

    ok no way to push changes to running notebook

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -104,7 +104,6 @@ def display_instances(image, boxes, masks, class_ids, class_names,
         print("\n*** No instances to display *** \n")
     else:
         assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
-
     # If no axis is passed, create one and automatically call show()
     auto_show = False
     if not ax:
